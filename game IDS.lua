c217eae3202bee0929bb584225e6232718a8f627
Jake1236552 <113944029+Jake1236552@users.noreply.github.com>
2023-01-06 15:34:47 +0300

    Add files via upload

---
+++ game IDS.lua
@@ -0,0 +1,8 @@
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Jake1236552/main/main/da%20hood.lua"))()
+elseif game.PlaceId == 11926383797 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Jake1236552/main/main/dah%20hood.lua"))()
+elseif game.PlaceId == 9744954014 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Jake1236552/main/main/tha%20hood%20script"))()
+
+end
